34120bfdcbe464300a9f77efe13a1bc877ae5e4a
hereisphil <thereisphil@gmail.com>
2026-05-04 14:21:38 -0600

    Final clean up.

--- index.test.js
+++ index.test.js
@@ -1,9 +1,5 @@
 import { obscureCreditCard } from "./index.js";
-// obscure cc# except last 4 digits
-// function parameter: string, returns: string
-// incoming string must be betweeen 12-16 digits and positive
 
-// TEST REVIEW: create a module that takes in prices in an array and returns the total
 describe("Credit Card Obscurification Tests", () => {
     test("Neg test: returns false if credit card number is less than 12 digits.", () => {
         expect(obscureCreditCard("12345678901234567")).toBeFalsy();
